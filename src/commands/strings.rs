b6e3637a5a15673ff8f2ce597a138847c6c1fee8
Vadim Popovych <zyzel19@gmail.com>
2026-04-21 20:10:10 +0200

    feat(commands): implement SET, GET, DEL, EXISTS, INCR and DECR

--- src/commands/strings.rs
+++ src/commands/strings.rs
@@ -1,1 +1,40 @@
 // SET, GET, DEL, EXISTS, INCR, DECR
+
+use crate::protocol::Reply;
+use crate::server::Shared;
+
+pub fn set(_shared: &Shared, key: String, value: String) -> Reply {
+    let store = &_shared.store;
+    store.set(key, value, None);
+    Reply::Simple("OK".to_string())
+}
+
+pub fn get(_shared: &Shared, key: String) -> Reply {
+    let store = &_shared.store;
+    match store.get(&key) {
+        Ok(x) => match x {
+            Some(x) => Reply::Bulk(x),
+            None => Reply::Nil
+        },
+        Err(m) => Reply::Error(m.to_string())
+    }
+}
+
+pub fn del(_shared: &Shared, key: String) -> Reply {
+    let store = &_shared.store;
+    Reply::Integer(store.del(&key) as i64)
+}
+
+pub fn exists(_shared: &Shared, key: String) -> Reply {
+    let store = &_shared.store;
+    Reply::Integer(store.exists(&key) as i64)
+}
+
+/// Used simultaneously for increment and decrement cmds, set `delta` to `1` or `-1`
+pub fn incr(_shared: &Shared, key: String, delta: i64) -> Reply {
+    let store = &_shared.store;
+    match store.incr(&key, delta) {
+        Ok(x) => Reply::Integer(x),
+        Err(m) => Reply::Error(m.to_string())
+    }
+}

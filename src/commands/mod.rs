f3764bdbca45236fbe919f610b0e5ac3f19d763f
Vadim Popovych <zyzel19@gmail.com>
2026-04-21 20:42:32 +0200

    refactor: rename _shared to shared

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -6,16 +6,16 @@ use crate::protocol::{Command, Reply};
 use crate::server::Shared;
 use std::sync::Arc;
 
-pub fn execute(cmd: Command, _shared: &Arc<Shared>) -> Reply {
+pub fn execute(cmd: Command, shared: &Arc<Shared>) -> Reply {
     match cmd {
         Command::Ping(msg) => misc::ping(msg),
         Command::Echo(msg) => misc::echo(msg),
-        Command::Get { key } => strings::get(_shared, key),
-        Command::Set { key, value } => strings::set(_shared, key, value),
-        Command::Del { key } => strings::del(_shared, key),
-        Command::Exists { key } => strings::exists(_shared, key),
-        Command::Incr { key } => strings::incr(_shared, key, 1),
-        Command::Decr { key } => strings::incr(_shared, key, -1),
+        Command::Get { key } => strings::get(shared, key),
+        Command::Set { key, value } => strings::set(shared, key, value),
+        Command::Del { key } => strings::del(shared, key),
+        Command::Exists { key } => strings::exists(shared, key),
+        Command::Incr { key } => strings::incr(shared, key, 1),
+        Command::Decr { key } => strings::incr(shared, key, -1),
         Command::Expire { .. } => Reply::Nil,
         Command::Ttl { .. } => Reply::Nil,
     }
--- src/commands/strings.rs
+++ src/commands/strings.rs
@@ -3,14 +3,14 @@
 use crate::protocol::Reply;
 use crate::server::Shared;
 
-pub fn set(_shared: &Shared, key: String, value: String) -> Reply {
-    let store = &_shared.store;
+pub fn set(shared: &Shared, key: String, value: String) -> Reply {
+    let store = &shared.store;
     store.set(key, value, None);
     Reply::Simple("OK".to_string())
 }
 
-pub fn get(_shared: &Shared, key: String) -> Reply {
-    let store = &_shared.store;
+pub fn get(shared: &Shared, key: String) -> Reply {
+    let store = &shared.store;
     match store.get(&key) {
         Ok(x) => match x {
             Some(x) => Reply::Bulk(x),
@@ -20,19 +20,19 @@ pub fn get(_shared: &Shared, key: String) -> Reply {
     }
 }
 
-pub fn del(_shared: &Shared, key: String) -> Reply {
-    let store = &_shared.store;
+pub fn del(shared: &Shared, key: String) -> Reply {
+    let store = &shared.store;
     Reply::Integer(store.del(&key) as i64)
 }
 
-pub fn exists(_shared: &Shared, key: String) -> Reply {
-    let store = &_shared.store;
+pub fn exists(shared: &Shared, key: String) -> Reply {
+    let store = &shared.store;
     Reply::Integer(store.exists(&key) as i64)
 }
 
 /// Used simultaneously for increment and decrement cmds, set `delta` to `1` or `-1`
-pub fn incr(_shared: &Shared, key: String, delta: i64) -> Reply {
-    let store = &_shared.store;
+pub fn incr(shared: &Shared, key: String, delta: i64) -> Reply {
+    let store = &shared.store;
     match store.incr(&key, delta) {
         Ok(x) => Reply::Integer(x),
         Err(m) => Reply::Error(m.to_string()),
